52968b3f1caea9344c4b2077cec8d7b6096eb0c8
Alex C. Martins <cantareiramartins@gmail.com>
2022-10-12 01:06:39 +0100

    creating partials, statics and changing config

--- jewelry/views.py
+++ jewelry/views.py
@@ -1,19 +1,18 @@
-
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'jewelry/pages/home.html')
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'jewelry/pages/contact.html')
 
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'jewelry/pages/login.html')
 
 
 def jewel(request):
-    return render(request, 'jewel.html')
+    return render(request, 'jewelry/pages/jewel.html')
