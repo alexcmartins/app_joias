f76b6b1fea455eadf06940f0d82274c511de84ba
Alex C. Martins <cantareiramartins@gmail.com>
2022-11-09 02:09:52 +0000

    Function search contacts in the home

--- jewelry/views.py
+++ jewelry/views.py
@@ -8,34 +8,37 @@ from .forms import NewContactForm
 from .models import Contacts
 
 
-# Create your views here.
+# Home - List all contacts and search
 def home(request):
-    search_term = request.GET.get('search', '')
-    print(search_term)
-    contacts_filters = Contacts.objects.filter(
-        Q(first_name__icontains=search_term) |
-        Q(last_name__icontains=search_term),
-    ).order_by('-id')
-    print(contacts_filters)
-    contacts = Contacts.objects.all().order_by('-id')
+    if request.method == "GET":
+        search_term = request.GET.get('search', '')
+        if search_term != '':
+            print(search_term)
+            contacts_filters = Contacts.objects.filter(
+                Q(first_name__icontains=search_term) |
+                Q(last_name__icontains=search_term),
+            ).order_by('-id')
+            print(contacts_filters)
 
-    return render(request, 'jewelry/pages/home.html', context={
-        # 'first_name': f'Search for"{search_term}" |',
-        'search_term': search_term,
-        'contacts': contacts,
-        'contacts_filters': contacts_filters,
-    })
+            return render(request, 'jewelry/pages/home.html', context={
+                'first_name': f'Search for"{search_term}" |',
+                'search_term': search_term,
+                'contacts_filters': contacts_filters,
+            })
+        else:
+            contacts = Contacts.objects.all().order_by('-id')
+            return render(request, 'jewelry/pages/home.html', context={
+                'contacts': contacts,
+            })
 
 
+# View contact
 def contact(request, id):
     contact = Contacts.objects.filter(id=id).order_by('-id').first()
     return render(request, 'jewelry/pages/contact.html', context={
         'contact': contact,
     })
 
-
-# def new_contact(request):
-#   return render(request, 'jewelry/pages/newcontact.html')
 
 # Create new contact
 def new_contact(request):
@@ -51,75 +54,49 @@ def new_contact(request):
         if 'submitted' in request.GET:
             submitted = True
 
-    return render(request, 'jewelry/pages/newcontact.html', context={
+    return render(request, 'jewelry/pages/new_contact.html', context={
         'form': form,
         'submitted': submitted,
     })
 
-    """if request.method == "POST":
-        first_name = request.POST.get("inputFirstName")
-        last_name = request.POST.get("inputLastName")
-        email = request.POST.get("inputEmail")
-        company = request.POST.get("inputCompany")
-        birthday = request.POST.get("inputDate")
-        instagram = request.POST.get("inputInstagram")
-        whatsapp = request.POST.get("checkboxWhatsapp")
-        mobile = request.POST.get("inputMobile")
-        house_office = request.POST.get("inputHouseOffice")
-        image_contact = request.POST.get("inputImage")
-        address = request.POST.get("inputAddress")
-        notes = request.POST.get("inputNotes")
 
-        print(new_contact)
-
-        Contacts(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            company=company,
-            birthday=birthday,
-            instagram=instagram,
-            whatsapp=whatsapp,
-            mobile=mobile,
-            house_office=house_office,
-            image_contact=image_contact,
-            address=address,
-            notes=notes,
-        ).save()
-        print(Contacts)
-
-        return HttpResponseRedirect('/')
-    else:
-        return render(request, 'jewelry/pages/newcontact.html')"""
+# Update contact
+def update_contact(request, id):
+    contact = Contacts.objects.filter(id=id).order_by('-id').first()
+    return render(request, 'jewelry/pages/update_contact.html', context={
+        'contact': contact,
+    })
 
 
-def updatecontact(request):
-    return render(request, 'jewelry/pages/updatecontact.html')
-
-
+# Login
 def login(request):
     return render(request, 'jewelry/pages/login.html')
 
 
+# List all jewelry
 def jewelry(request):
     return render(request, 'jewelry/pages/jewelry.html', context={
         'jewelry': [generator_jewel() for _ in range(30)],
     })
 
 
+# View jewel
 def jewel(request, id):
     return render(request, 'jewelry/pages/jewel.html', context={
         'jewel': generator_jewel(),
     })
 
 
-def newjewel(request):
-    return render(request, 'jewelry/pages/newjewel.html')
+# Create new jewel
+def new_jewel(request):
+    return render(request, 'jewelry/pages/new_jewel.html')
 
 
-def updatejewel(request):
-    return render(request, 'jewelry/pages/updatejewel.html')
+# Update jewel
+def update_jewel(request):
+    return render(request, 'jewelry/pages/update_jewel.html')
 
 
-def settingsjewel(request):
-    return render(request, 'jewelry/pages/settingsjewel.html')
+# Settings jewelry
+def settings_jewel(request):
+    return render(request, 'jewelry/pages/settings_jewel.html')
